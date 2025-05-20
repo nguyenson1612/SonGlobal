a99d0e0c0614f5469e65ac99aad01ab81e97cd7b
minhson <minhson1812003@gmail.com>
2025-05-21 06:38:44 +0700

    buttons

--- app/buttons/page.tsx
+++ app/buttons/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 const ButtonsPage = () => {
     return (
-        <div>   
-       Buttons page!
+        <div className="p-4 space-y-4 flex flex-col max-w-[200px]"> 
+            <Button>Primary</Button>
+            <Button>Primary Outline</Button>
         </div>
     );
 };
